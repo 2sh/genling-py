2e439acedc7d8b6ea60f0437855ce5a2f0c79ccc
Seán <git@2sh.me>
2015-06-09 19:47:18 +0100

    Reorganise application as library
    Merge stem.py and writing_system.py and replace genling.py
    Convert example configs into Python scripts

--- README.md
+++ README.md
@@ -1,22 +1,15 @@
 #Generatrum Linguarum
 
-This is a project to develop an application with which it is possible to generate a spoken contructed language, a conlang. In its current state, it is simply a word stem generator.
+This is a project to develop a Python library with which it is possible to generate a spoken constructed language, a conlang. In the library's current state, it is possible to program a word stem generator.
 
-In the generating process, user input weights and probabilities affect the usage of individual phonemes, usage of syllable segments, usage of syllables in the same position, the amount of syllables within a stem and whether a stem caught by a filter is to be allowed. User input regex is used for deciding whether a generated word stem is allowed or not (e.g. combinations of phonemes that are not permitted) and in transliterating a generated word/stem from a raw form (e.g. "&lt;ge&gt;&lt;hYo&gt;&lt;uN&gt;&lt;e&gt;&lt;nu&gt;") into a final form (e.g. "gehyōn'enu", "げひょうんえぬ", "gexyoungënu" or "гэхёунъэну").
-
-For a quicker development of the design, the application is written in Python. Any optimisation or porting to a faster language can be done later if at all necessary.
+In the generating process, weights and probabilities affect the usage of individual phonemes, usage of syllable segments, usage of syllables in the same position, the amount of syllables within a stem and whether a stem caught by a filter is to be allowed. Regex is used for deciding whether a generated word stem is allowed or not (e.g. combinations of phonemes that are not permitted) and in transliterating a generated word/stem from a raw form (e.g. "&lt;ge&gt;&lt;hYo&gt;&lt;uN&gt;&lt;e&gt;&lt;nu&gt;") into a final form (e.g. "gehyōn'enu", "げひょうんえぬ", "gexyoungënu" or "гэхёунъэну").
 
 Requirements:
 * Python3
-* PyYAML - A language is defined with a YAML config file
 
-##Current usage
+###Examples
 ````
-python3 genling.py <language config> <writing system index> <word count>
-````
-The writing system index starts with 1 and is the definition order within the config file. 0 keeps the stem in its raw form.
-
-###Example
-````
-python3 genling.py config/example2.yaml 1 100
+python3 conlang.py 100 raw
+python3 conlang.py 100
+python3 japanese.py 100 hiragana
 ```
--- genling.py
+++ genling.py
@@ -1,95 +1,147 @@
-#! /usr/bin/env python3
 #
-#	Copyright 2014 2sh <contact@2sh.me>
-#	
+#	Generatrum Linguarum
+#	Copyright (C) 2014-2015 2sh <contact@2sh.me>
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#	
+#
 
-from stem import *
-from writing_system import *
-from yaml import load as yaml_decode
-from sys import argv
+from rand import weighted_choice
+from random import random
+from re import compile as re_compile
 
-stem_config = argv[1]
-writing_system_index = int(argv[2])
-stem_count = int(argv[3])
+class Phoneme:
+	def __init__(self, grapheme, weight=1):
+		self.grapheme = grapheme
+		self.weight = weight
 
-with open(stem_config) as f:
-	stem = yaml_decode(f)
+class Segment:
+	def __init__(self, phonemes, **prop):
+		self.phonemes = phonemes
+		self.probability = prop.get("probability", 1.0)
+		self.prefix = prop.get("prefix", "")
+		self.suffix = prop.get("suffix", "")
 
-syllables = list()
-for syllable in stem["syllables"]:
-	if "segments" not in syllable:
-		continue
-	
-	segments = list()
-	for segment in syllable["segments"]:
-		if "phonemes" not in segment:
-			continue
-		
-		phonemes = list()
-		for phoneme in segment["phonemes"]:
-			if isinstance(phoneme, str):
-				phoneme = [phoneme]
-			try:
-				phonemes.append(Phoneme(*phoneme))
-			except:
-				print("Phoneme '" + phoneme + "' invalid.")
-		
-		del segment["phonemes"]
-		segments.append(Segment(phonemes, **segment))
-	
-	del syllable["segments"]
-	syllables.append(Syllable(segments, **syllable))
+	def generate(self):
+		weights = [phoneme.weight for phoneme in self.phonemes]
+		return self.prefix + self.phonemes[weighted_choice(weights)].grapheme + self.suffix
 
+class Syllable:
+	def __init__(self, segments, **prop):
+		self.segments = segments
+		self.position = prop.get("position", 0)
+		self.weight = prop.get("weight", 1)
+		self.prefix = prop.get("prefix", "")
+		self.suffix = prop.get("suffix", "")
+		self.infix = prop.get("infix", "")
 
-filters = list()
-for filt in stem.get("filters", []):
-	if isinstance(filt, str):
-		filt = [filt]
-	try:
-		filters.append(RegexFilter(*filt))
-	except:
-		print("Filter '" + str(filt) + "' invalid.")
-		
-stem["filters"] = filters
+	def is_allowed_postion(self, i, l):
+		if self.position == 0:
+			return True
 
-writing_systems = list()
-for writing_system in stem["writing-systems"]:
-	if "transliterations" not in writing_system:
-		continue
-		
-	transliterations = list()
-	for transliteration in writing_system["transliterations"]:
-		if isinstance(transliteration, str):
-			transliteration = [transliteration]
-		try:
-			transliterations.append(RegexTransliteration(*transliteration))
-		except:
-			print("Transliteration '" + transliterations + "' invalid.")
-			
-	writing_systems.append(WritingSystem(transliterations))
-	
-del stem["syllables"]
-stem = Stem(syllables, **stem)
+		if isinstance(self.position, int):
+			position = self.position if self.position > 0 else l+self.position+1
 
-for i in range(stem_count):
-	string = stem.generate()
-	if writing_system_index > 0:
-		string = writing_systems[writing_system_index-1].transliterate(string)
-	if string:
-		pass
-		print(string)
-	else:
-		print("Failed to generate stem. Please check the filters.")
+			if self.position == (i+1):
+				return True
+
+		elif(not isinstance(self.position, basestring) and
+				len(self.position) > 1):
+			minpos = self.position[0] if self.position[0] > 0 else l+self.position[0]+1
+			maxpos = self.position[1] if self.position[1] > 0 else l+self.position[1]+1
+
+			if minpos <= (i+1) <= maxpos:
+				return True
+
+		return False
+
+	def generate(self):
+		string = [segment.generate() for segment in self.segments if random() <= segment.probability]
+		return self.prefix + self.infix.join(string) + self.suffix
+
+class Stem:
+	def __init__(self, syllables, **prop):
+		self.syllables = syllables
+		self.balance = prop.get("balance", [1])
+		self.prefix =  prop.get("prefix", "")
+		self.suffix = prop.get("suffix", "")
+		self.infix = prop.get("infix", "")
+
+	def generate(self):
+		syllable_amount = weighted_choice(self.balance) + 1
+
+		string = list()
+		for i in range(syllable_amount):
+			syllables = [syllable for syllable in self.syllables if syllable.is_allowed_postion(i, syllable_amount)]
+			if len(syllables) > 1:
+				weights = [syllable.weight for syllable in syllables]
+				syllable = self.syllables[weighted_choice(weights)]
+			else:
+				syllable = syllables[0]
+
+			string.append(syllable.generate())
+
+		return self.prefix + self.infix.join(string) + self.suffix
+
+class RegexFilter:
+	def __init__(self, pattern, probability=1.0, allow=False):
+		self.pattern = pattern
+		self.probability = probability
+		self.allow = allow
+
+	def is_allowed(self, stem_string):
+		if random() > self.probability:
+			return not self.allow
+		if self.regex.search(stem_string):
+			return self.allow
+		else:
+			return not self.allow
+
+	@property
+	def pattern(self):
+		return self._pattern
+
+	@pattern.setter
+	def pattern(self, pattern):
+		self._pattern = pattern
+		self.regex = re_compile(pattern)
+
+class RegexTransliteration:
+	def __init__(self, pattern, replacement, probability=1.0):
+		self.pattern = pattern
+		self.replacement = replacement
+		self.probability = probability
+
+	def transliterate(self, string):
+		if random() > self.probability:
+			return string
+
+		return self.regex.sub(self.replacement, string)
+
+	@property
+	def pattern(self):
+		return self._pattern
+
+	@pattern.setter
+	def pattern(self, pattern):
+		self._pattern = pattern
+		self.regex = re_compile(pattern)
+
+class WritingSystem:
+	def __init__(self, transliterations):
+		self.transliterations = transliterations
+
+	def transliterate(self, string):
+		for transliteration in self.transliterations:
+			string = transliteration.transliterate(string)
+		return string
